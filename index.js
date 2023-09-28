055bbd778c34515fa3d74e7113c7fbd344f05323
rohitbisht01 <rb9519208@gmail.com>
2023-09-28 19:30:38 +0530

    whitelisted url

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+const isDev = app.settings.env === "developement";
+const URL = isDev
+  ? `http://localhost:3000`
+  : `https://sketchbook-m9f5yilt6-rohitbisht01.vercel.app`;
+app.use(cors({ origin: URL }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: "http://localhost:3000",
+  cors: URL,
 });
-
 io.on("connection", (socket) => {
   console.log("server connected");
+
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
+
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  });
+
+  socket.on("changeConfig", (arg) => {
+    socket.broadcast.emit("changeConfig", arg);
+  });
 });
 
 httpServer.listen(4000);
